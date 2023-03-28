391bf09fdddd849006bc9d1a7bbc80a17bb8636f
Yuneth <yunethperera987@gmail.com>
2023-03-28 19:58:22 +0530

    Add project files.

---
+++ eCommerce.Entities/CustomEntities/CategoryWithChildren.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Entities.CustomEntities
+{
+    public class CategoryWithChildren
+    {
+        public Category Category { get; set; }
+        public List<Category> Children { get; set; }
+
+        
+    }
+}
